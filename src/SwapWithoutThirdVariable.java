ce88d7d410968cf7eb804950e568486a81cee319
Madhuri Dhananjeyan <madhurao753@gmail.com>
2025-09-16 18:07:16 +0530

    Initial commit - SwapNumbers project

---
+++ src/SwapWithoutThirdVariable.java
@@ -0,0 +1,11 @@
+public class SwapWithoutThirdVariable {
+    public static void main (String[] args) {
+        int a = 5;
+        int b = 10;
+        System.out.println("Before Swap: a =" + a + ", b = " +b);
+        a = a+b;
+        b = a-b;
+        a=a-b;
+        System.out.println("After swap: a =" + a + ", b = " +b);
+    }
+}
